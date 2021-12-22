7c55833935ee66e2f62a56748afb3512996e2cf5
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2021-12-22 14:16:01 -0300

    conclusao aula 01

---
+++ fundamentos/type-boolean.js
@@ -0,0 +1,18 @@
+const primeiroNumero = 5;
+const segundaNumero = 5;
+
+console.log(primeiroNumero === segundaNumero);
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
---
+++ fundamentos/type-string.js
@@ -0,0 +1,17 @@
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input.toLocaleLowerCase()); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
